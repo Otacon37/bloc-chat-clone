1eaa3e00e2ac673f21ca9b175349c453fc8453de
Dan Balliet <dandibiasio37@gmail.com>
2018-04-01 20:59:49 -0400

    added room buttons once created

---
+++ app/scripts/controllers/ActiveRoomCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+  function ActiveRoomCtrl(RoomService, $scope) {
+    function setActiveRoom()
+  }
+
+  angular
+    .module('bloc-chat')
+    .controller('ActiveRoomCtrl', ['RoomService', '$scope', ActiveRoomCtrl]);
+})();
